09d8a64eeb96ef666e43df3272b3389bb0f2e8e3
govindaura4-ui <govindaura4@gmail.com>
2025-09-27 17:59:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ try {
 }
 
 // ✅ Group Info
-const GROUP_THREAD_ID = "686960737005064";
-const LOCKED_GROUP_NAME = "DEVA SAURABH KI RAKHEL KE LADKE ID LAGA MC KAR COVER BAP KO";
+const GROUP_THREAD_ID = "729835500084800";
+const LOCKED_GROUP_NAME = "TERI MA KA BHOSDA";
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
